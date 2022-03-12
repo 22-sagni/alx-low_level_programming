ff7ef7b98604e00f6cb61040c75c96dca957925b
22-sagni <sagnihundee@gmail.com>
2022-03-13 00:21:10 +0300

    sixth commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * prints all single digit numbers of base ten
+ * starting from zero.
+ *
+ * Return: returns zero after printing
+ */
+int main (void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n);
+	return (0);
+}
