8e55b3a70b79ca7fd8a8b4c2627f3e3c09bd0c50
22-sagni <sagnihundee@gmail.com>
2022-03-12 18:11:08 +0300

    yet another correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * this program initiates at main
+ * and terminates at the return statement
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
