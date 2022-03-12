389083b3c78d9a4bb51791222da6e0094be4ee08
22-sagni <sagnihundee@gmail.com>
2022-03-12 18:22:05 +0300

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - entry point 
- * 
+ * main - entry point
+ *
  * return: 0 after executing the function
  */
-int main(void)
+int main(void)/* return: 0 after execution the function*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
