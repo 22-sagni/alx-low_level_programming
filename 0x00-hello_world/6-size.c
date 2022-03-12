14dc31b11330a705ecc73e43547b12d60de9f5fb
22-sagni <sagnihundee@gmail.com>
2022-03-12 19:07:56 +0300

    seventh commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *Main - entry point
+ *
+ *Return:0 after printing the function
+ */
+int main(void)
+{
+	printf("Size of a char:" %d, sizeof(char));
+	printf("Size of an int:" %d, sizeof(int));
+	printf("Size of a long int:" %d, sizeof(long int));
+	printf("Size of a long long int:" %d, sizeof(long long int));
+	printf("Size of a float:" %d, sizeof(float));
+	return (0);
+}
