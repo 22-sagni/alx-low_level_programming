990a58525f4f8f549e90760d46d6fb6ed68d47ed
22-sagni <sagnihundee@gmail.com>
2022-03-13 10:36:27 +0300

    nineth task commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entrance
+ *
+ * prints base sixteen numbers 
+ * in lower case
+ *
+ * Return: returns 0 upon success
+ */
+int main(void)
+{
+	char ch;
+	int i;
+
+	for (i = 48; i < 57; i++)
+		putchar(i);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		purchar(ch);
+	putchar('\n');
+	return (0);
+}
