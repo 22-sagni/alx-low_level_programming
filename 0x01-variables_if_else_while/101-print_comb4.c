cef1388d130a0f2e8f24237a5489947e3023529e
22-sagni <sagnihundee@gmail.com>
2022-03-13 18:39:53 +0300

    yer another commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entrance
+ *
+ * prints single digit combinaltions
+ *
+ * Return: returns zero upon success
+ */
+int main(void)
+{
+	int i, m, n;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (m = 48; m <= 57; m++)
+		{
+			for (n = 48; n <= 57; n++)
+			{
+				if (m <= i)
+					continue;
+				if (n <= m)
+					continue;
+
+				putchar(i);
+				putchar(m);
+				putchar(n);
+
+				if (i == 57)
+					break;
+
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
+
