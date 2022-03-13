ea28f64687c41b1ba9ac23a72da56a5882516397
22-sagni <sagnihundee@gmail.com>
2022-03-13 11:49:15 +0300

    tenth commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 58;i++)
+	for (i = 48; i <= 58; i++)
 	{
 		putchar(i);
+
 		if (i == 58)
+		{
 			break;
+		}
 		putchar(',');
 		putchar(' ');
+		putchar('\n');
 	}
 	
 	return (0);
