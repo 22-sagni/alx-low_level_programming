4c587eb96aca5d813cf8b91dfb2c327a1eb726c3
22-sagni <sagnihundee@gmail.com>
2022-03-13 18:03:15 +0300

    correction

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,14 +14,14 @@ int main(void)
 	for (i = 48; i <= 57; i++)
 	{
 	for (m = 48; i <= 57; i++)
-	if (i == m)
-	continue;
-	if (m < i)
-	continue;
+		if (i == m)
+		continue;
+		if (m < i)
+		continue;
 	putchar(i);
 	putchar(m);
-	if (i == 56)
-	break;
+		if (i == 56)
+		break;
 
 	putchar(',');
 	putchar(' ');
