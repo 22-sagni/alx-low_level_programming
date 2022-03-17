dbf4a371a8e623a561a64114126c193b4b0d87ae
22-sagni <sagnihundee@gmail.com>
2022-03-17 20:41:23 +0300

    eleventh commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ *
+ * @n: parameter to be evaluated
+ * @m: parameter to be evaluated
+ *
+ * Return: returns the result
+ */
+int add(int m, int n)
+{
+	return (m +n);
+}
