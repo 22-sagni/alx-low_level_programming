664aeef505cedada7bb67f71a477b2d526a9be68
22-sagni <sagnihundee@gmail.com>
2022-03-17 18:18:55 +0300

    seventh commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value of an integer
+ *
+ * Return: returns an integer upon completion
+ */
+
+int _abs(int)
+{
+	if (int < 0)
+	{
+		_putchar(unsigned int);
+		return (0);
+	}
+	else
+	{
+		_putchar(int);
+		return (1);
+	}
+}
