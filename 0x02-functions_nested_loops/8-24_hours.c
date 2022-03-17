8aaed65d4878f0e47399f293b6317c88f419035b
22-sagni <sagnihundee@gmail.com>
2022-03-17 20:17:31 +0300

    correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,19 +9,19 @@ void jack_bauer(void)
 {
 	int i, j, m, n;
 
-	if (i =48; i <= 57; i++)
+	if (i == 48; i <= 57; i++)
 	{
-		for (j = 48; j <= 57; j++)
+		for (j == 48; j <= 57; j++)
 		{
-			if (m = 48; m <= 57; m++)
+			if (m == 48; m <= 57; m++)
 			{
-				for (n = 48; n <= 57; n++)
+				for (n == 48; n <= 57; n++)
 				{
 					if ((i > 2 && j > 4))
 						continue;
 					if ((m > 53 && n > 57))
 						continue;
-					if ((i + j + m + n = 19))
+					if ((i + j + m + n == 19))
 						break;
 					_putchar(i);
 					_putchar(j);
