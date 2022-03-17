d443bd95f8dbf9abf000a59bcaefe415bc9c525a
22-sagni <sagnihundee@gmail.com>
2022-03-17 20:34:29 +0300

    tenth commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * times_table - prints 9 times table starting with 0
+ *
+ * Return: returns nothing
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			putchar(i * j);
+		}
+	}
+}
+
