20b7659e475e28f35f6c963a35309c6891171346
22-sagni <sagnihundee@gmail.com>
2022-03-18 12:41:23 +0300

    correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,18 +4,44 @@
  *
  * Return: returns nothing
  */
-
 void times_table(void)
 {
-	int i, j, k;
-
-	for (i = 0; i <= 9; i++)
+	int a;
+	int b;
+	int c;
+	for (a = 0; a <= 9; a++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (b = 0; b <= 9; b++)
 		{
-			k = i * j;
-			_putchar(k);
+			c = a * b;
+			if ((c / 10) == 0)
+			{
+				if (b == 0)
+				{
+					_putchar ('0');
+				}
+				if (b != 0)
+				{
+					_putchar (' ');
+					_putchar ((c % 10) + '0');
+				}
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
+			}
+			else
+			{
+				_putchar ((c / 10) + '0');
+				_putchar ((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
+			}
 		}
+		_putchar ('\n');
 	}
 }
-
