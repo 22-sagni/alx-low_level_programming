b9bfe45ef921602b39ca1e914db74a421870d12c
22-sagni <sagnihundee@gmail.com>
2022-03-19 22:37:33 +0300

    correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,22 +7,23 @@
  **/
 void print_times_table(int n)
 {
-	int i, j;
-
-	if (n > 15)
-	{
-		break;
-	}
+	int i, j, k;
 
 	for (i = 0; i < n + 1; i++)
 	{
 		for (j = 0; j < n + 1; j++)
 		{
-			Printf("%d ", i * j);
+			if (n > 15)
+			{
+				break;
+			}
+
+			k = i * j
+			_putchar(k);
 
 			if (j < n)
 			{
-				printf(", ");
+				_putchar(", ");
 			}
 		}
 	}
