c3715d80236acb2a74239c2900120812f8e5cfc6
22-sagni <sagnihundee@gmail.com>
2022-03-25 14:18:31 +0300

    due commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0 to hundred
+ * replacing multiples of 3, 5,  and both
+ * with string
+ *
+ * Return: always returns zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && !(i % 5 == 0))
+		{
+			Printf("Fizz");
+		}
+		else if ((i % 5 == 0) && !(i % 3 == 0))
+		{
+			printf("Buzz");
+		}
+		else if ((i % 5 == 0) && (i % 3 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	return (0);
+}
+
