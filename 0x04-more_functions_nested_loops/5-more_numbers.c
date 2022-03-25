bdacd078497426a3f3b058f9ef96f4649672296c
22-sagni <sagnihundee@gmail.com>
2022-03-25 13:17:11 +0300

    correciton

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,26 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k, l;
 
-	for (i >= 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		j = 0;
-		while (j < 15);
+		j = 48;
+		while (j < 58);
 		{
-			_putchar(j + '0);
+			_putchar(j + '0');
 			j++;
 		}
+
+		for (k = 49; k < 50; k++)
+		{
+			l = 48;
+			while (l < 53)
+			{
+				_putchar(k + '0');
+				_putchar(l + '0');
+				l++;
+			}
 		_putchar('\n');
 	}
 	_putchar('\n');
