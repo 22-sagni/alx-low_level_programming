4afcdcddcaf6d8dcaf1fa9dbacc7d5e156b3638c
22-sagni <sagnihundee@gmail.com>
2022-03-25 13:44:19 +0300

    due commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - function to print square
+ *
+ * @size: size input
+ * Return: no return value
+ */
+
+void print_square(int size)
+{
+	int m, n;
+
+	m = size;
+	n = size;
+	for (m = 1; m < m + 1; m++)
+	{
+		if ((m = 48) || (n = 48))
+			continue;
+		n = 1;
+		while (n < n+1)
+		{
+			_putchar('#');
+			n++;
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
