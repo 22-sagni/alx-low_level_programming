32f2ab81433ca090f6e9160b93213f392a4a49eb
22-sagni <sagnihundee@gmail.com>
2022-03-25 15:47:44 +0300

    correction

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - recet value of input using pionter
+ *
+ * @n: input parameter
+ * REturn: returns nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
