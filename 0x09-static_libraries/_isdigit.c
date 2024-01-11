fe8edfb59ec401f1be651be6d675ada5a1181857
22-sagni <sagnihundee@gmail.com>
2024-01-12 01:50:12 +0400

    main.h

---
+++ 0x09-static_libraries/_isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _isdigit - checks wether digit or not.
+ * @c - parameter to be checked.
+ *
+ * Return: integer.
+ */
+int _isdigit(int c)
+{
+	return (isdigit(c));
+}
