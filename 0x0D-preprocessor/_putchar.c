46a188c52c4b7182dea813915d8695142d98da6b
22-sagni <sagnihundee@gmail.com>
2024-01-22 17:11:45 +0400

    correction

---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (putchar(c));
+}
