d497d63d2cb4b0225ee006702a2c353d232da594
22-sagni <sagnihundee@gmail.com>
2024-01-24 22:18:50 +0400

    commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,10 @@
 #ifndef _HEADER_
 #define _HEADER_
 #include <stdlib.h>
+#include <stddef.h>
 
+int _putchar(char c);
+int _atoi(char *s);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
