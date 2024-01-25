c073544315df4081da7f671ce42af7f024c49855
22-sagni <sagnihundee@gmail.com>
2024-01-25 22:38:29 +0400

    correction once again

--- 0x10-variadic_function/0-sum_them_all.c
+++ 0x10-variadic_function/0-sum_them_all.c
@@ -8,17 +8,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int j;
+	unsigned int i;
 	int sum = 0;
-
-	if (n == 0)
-		return (0);
 
 	va_list list;
 
 	va_start(list, n);
 
-	for (j = 0; j < n; j++)
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
 		sum += va_arg(list, int);
 	va_end(list);
 	return (sum);
